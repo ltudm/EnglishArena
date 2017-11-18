f0fe925d18518c4c4f7fa8ed1d4af061431490da
vuhuykhanh <>
2017-11-19 00:18:06 +0700

    up

--- GuiClient/src/GUI/trochoi.java
+++ GuiClient/src/GUI/trochoi.java
@@ -332,7 +332,6 @@ public class trochoi extends JFrame {
 					data_sent.action="traloitruoc";
 					data_sent.data=data;					
 					messout(data_sent.action);
-					
 					//System.out.println(data[1]+data[2]+data[3]+data[4]+data[0]);
 					try
 					{
@@ -355,7 +354,7 @@ public class trochoi extends JFrame {
 						{				
 							messout("tra loi sau");
 							guidapan();
-							//rootPane.setEnabled(false);
+							btkt.setEnabled(false);
 						}
 							
 				//System.out.println(dapan);
@@ -467,7 +466,8 @@ public class trochoi extends JFrame {
 		));
 		
 		lbch4.setVisible(false);
-		
+		JRootPane rootPane = SwingUtilities.getRootPane(btkt); 
+		rootPane.setDefaultButton(btkt);
 		
 	}
 	public void huybo()
